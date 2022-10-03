6cd37a598a369592e0664b2c0ce58ad2794e7261
masud rana <masudranaabc@gmail.com>
2022-10-04 03:42:56 +0600

    added count state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
+}
+function Counter(){
+  const [count, setCount]=useState(20)
+  const incersCout=()=>setCount(count+1)
+  const dicresCout=()=>setCount(count-1)
+  return(
+    <div>
+      <h3>Count:{count}</h3>
+      <button onClick={incersCout}>Incres</button>
+      <button onClick={dicresCout}>dicres</button>
+    </div>
+  )
 }
 
 export default App;
